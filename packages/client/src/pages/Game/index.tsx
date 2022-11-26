6e4bbe859b068abbd291e0b0494b0120b4e86035
Oleg <olaskov@gmail.com>
2022-11-26 23:50:11 +0200

    added GamePage, CanvasComponent and imagees; changed title

---
+++ packages/client/src/pages/Game/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CanvasComponent from '../../components/Canvas';
+
+const GamePage: React.FC = () => {
+	return (
+		<div>
+			<CanvasComponent/>
+		</div>
+	);
+}
+
+export default GamePage;
