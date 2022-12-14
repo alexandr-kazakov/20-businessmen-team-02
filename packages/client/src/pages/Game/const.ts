97896d4a8e85d64608e22cb82c29945b947242c8
opv1 <pas7273@gmail.com>
2022-12-14 21:38:23 +0300

    fix: правка логики fullscreen api

--- packages/client/src/pages/Game/const.ts
+++
@@ -1,1 +0,0 @@
-export const eventCodes = ['ControlLeft', 'KeyQ']
--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -2,7 +2,6 @@ import React, { FC, useEffect, useRef, useState } from 'react'
 import CanvasComponent from '@/components/Canvas'
 import Button from '@/components/UI/Button'
 import { ButtonStyles } from '@/components/UI/Button/types'
-import { eventCodes } from './const'
 import styles from './styles.module.scss'
 
 const GamePage: FC = () => {
@@ -10,7 +9,6 @@ const GamePage: FC = () => {
   const [initStart, setInitStart] = useState<number>(0)
   const [scores, setScores] = useState<number>(-1)
   const header = scores < 0 ? null : scores === 0 ? 'У Вас 0 очков' : `Поздравляем у Вас ${scores} очков!`
-  const pressedKey = new Set()
 
   const clickStart = () => {
     setInitStart(Date.now())
@@ -62,32 +60,18 @@ const GamePage: FC = () => {
   }
 
   const handlerKeyDown = (event: any) => {
-    pressedKey.add(event.code)
-
-    for (const code of eventCodes) {
-      if (!pressedKey.has(code)) {
-        return
+    if (event.ctrlKey && event.code === 'KeyQ') {
+      if (elementRef.current) {
+        toggleFullScreen(elementRef.current)
       }
     }
-
-    pressedKey.clear()
-
-    if (elementRef.current) {
-      toggleFullScreen(elementRef.current)
-    }
-  }
-
-  const handlerOnKeyUp = (event: any) => {
-    pressedKey.delete(event.code)
   }
 
   useEffect(() => {
     document.addEventListener('keydown', event => handlerKeyDown(event))
-    document.addEventListener('keyup', event => handlerOnKeyUp(event))
 
     return () => {
       document.removeEventListener('keydown', event => handlerKeyDown(event))
-      document.removeEventListener('keyup', event => handlerOnKeyUp(event))
     }
   }, [])
 
