974790138d324674f2f0e5aea9e2f4b0cd43232a
opv1 <pas7273@gmail.com>
2023-02-19 20:20:25 +0300

    fix: фикс

--- packages/client/src/pages/Profile/components/ProfileUserDataList/index.tsx
+++ packages/client/src/pages/Profile/components/ProfileUserDataList/index.tsx
@@ -13,22 +13,20 @@ export const ProfileUserDataList: React.FC = () => {
 
   const getListNodes = (user: IUser | null, view: boolean) =>
     user &&
-    Object.entries(profileForm).map(([key, value]) => {
-      return (
-        <li key={key} className={styles.item}>
-          <div className={styles.colLeft}>{value}</div>
-          <div className={styles.colRight}>
-            <input
-              type="text"
-              className={styles.userDataInput}
-              name={key}
-              defaultValue={user[key as IUserKey] || undefined}
-              disabled={view}
-            />
-          </div>
-        </li>
-      )
-    })
+    Object.entries(profileForm).map(([key, value]) => (
+      <li key={key} className={styles.item}>
+        <div className={styles.colLeft}>{value}</div>
+        <div className={styles.colRight}>
+          <input
+            type="text"
+            className={styles.userDataInput}
+            name={key}
+            defaultValue={user[key as IUserKey] || undefined}
+            disabled={view}
+          />
+        </div>
+      </li>
+    ))
 
   const listNodes = useMemo(() => getListNodes(userObj, profileView), [userObj, profileView])
 
