fbad83300d91fab2dedae6fc44f118893a19de59
y.kochergin <y.kochergin@rambler-co.ru>
2023-02-23 12:46:32 +0300

    fix: readme updated

--- init.js
+++ init.js
@@ -2,4 +2,7 @@ const fs = require('fs')
 
 fs.copyFileSync('.env.sample', '.env')
 
+fs.copyFileSync('.env.sample', 'packages/client/.env')
+fs.copyFileSync('.env.sample', 'packages/server/.env')
+
 fs.mkdirSync('tmp/pgdata', { recursive: true })
